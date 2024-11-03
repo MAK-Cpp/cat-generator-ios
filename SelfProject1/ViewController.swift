72df7a116f15e0f00c2f0bd47dd8913edcfae0a4
Primakov Maxim Antonovich <spartmenik@gmail.com>
2024-11-03 19:46:26 +0300

    init commit

---
+++ SelfProject1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SelfProject1
+//
+//  Created by Maxim Primakov on 29.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
